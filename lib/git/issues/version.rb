96c39012d1d97e7f7f1c1e9638c2cd9612161ed0
hexium310 <hexium310@gmail.com>
2018-02-02 18:31:38 +0900

    Refactor to make tests pass

--- lib/git/issues/version.rb
+++ lib/git/issues/version.rb
@@ -1,5 +1,5 @@
 module Git
   module Issues
-    VERSION = "0.1.0"
+    VERSION = '0.1.0'.freeze
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "git/issues"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'git/issues'
 
-require "minitest/autorun"
+require 'minitest/autorun'
